95511f26748e04611a7b05571e539b20dbfc27d7
Ismail <ismailshaik8007@gmail.com>
2023-12-19 16:51:48 +0530

    Hello world app using thymeleaf

---
+++ exampl_18/src/main/java/com/springbytes/exampl_18/Exampl18Application.java
@@ -0,0 +1,13 @@
+package com.springbytes.exampl_18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exampl18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exampl18Application.class, args);
+	}
+
+}
